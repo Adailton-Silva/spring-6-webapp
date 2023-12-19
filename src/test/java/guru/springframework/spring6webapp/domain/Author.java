9e23b5c1decbbbab221f93b2aa5ab95ab1b79527
linux-adailton <ntconsult.adailton@agi.com.br>
2023-12-19 15:23:29 -0300

    2-jpa-relationships

--- src/test/java/guru/springframework/spring6webapp/domain/Author.java
+++ src/test/java/guru/springframework/spring6webapp/domain/Author.java
@@ -16,6 +16,8 @@ public class Author {
     @ManyToMany(mappedBy = "authors")
     private Set<Book> books;
 
+
+
     public Set<Book> getBooks() {
         return books;
     }
@@ -46,5 +48,30 @@ public class Author {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return getId() != null ? getId().equals(author.getId()) : author.getId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId().hashCode() : 0;
     }
 }
